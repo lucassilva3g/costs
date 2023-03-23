08afcbe675b9b74c7467de46903bf0f8cec17d98
lucassilva3g <lucassantos.santos90@hotmail.com>
2023-03-23 12:29:12 -0300

    Inserindo dados no banco via API

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,14 +1,41 @@
-import ProjectForm from '../project/ProjectForm';
-import styles from './NewProject.module.css';
+import { useNavigate } from "react-router-dom";
+import ProjectForm from "../project/ProjectForm";
+import styles from "./NewProject.module.css";
+
+
+
 
 function NewProject() {
+  const history = useNavigate(); //para redirecionar para outra pagina
+  function createPost(project) {
+    //initialize cost and services
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        //redirect
+        history("/projects", { message: "Projeto criado com sucesso!"});
+      })
+
+      .catch((err) => console.log(err)); //then para receber uma resposta e catch recebe o erro e mostra no console
+  }
+
   return (
     <div className={styles.newproject_container}>
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-      <ProjectForm btnText='Criar Projeto' />
+      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
     </div>
   );
-}	
+}
 
 export default NewProject;
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,12 +1,14 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 import styles from "./ProjectForm.module.css";
 
-function ProjectForm({ btnText }) {
+
+function ProjectForm({ handleSubmit, btnText, projectData }) {
   const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {});
 
   // fetch("http://localhost:5000/categories", {
   //   method: "GET",
@@ -20,9 +22,10 @@ function ProjectForm({ btnText }) {
   //   })
 
   //   .catch((err) => console.log(err));  o react n entende essa forma e chama milhoes de vezes,  fazemdo um loop infinito
-// a froma certa é essa abaixo, usando o useEffect
+  // a froma certa é essa abaixo, usando o useEffect
   useEffect(() => {
-    fetch("http://localhost:5000/categories", { //usamos para pegar dados do backend
+    fetch("http://localhost:5000/categories", {
+      //usamos para pegar dados do backend
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -35,24 +38,49 @@ function ProjectForm({ btnText }) {
       .catch((err) => console.log(err));
   }, []);
 
+  const submit = (e) => {
+    e.preventDefault(); //para nao recarregar a pagina
+    //console.log(project);
+    handleSubmit(project); //chama a funcao que foi passada como props
+  };
+
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]: e.target.value });
+  }
+
+  function handleCategory(e) {
+    setProject({
+      ...project,
+      category: {
+        id: e.target.value,
+        name: e.target.options[e.target.selectedIndex].text
+    }});
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Nome do projeto"
         name="name"
         placeholder="Insira o nome do projeto"
+        handleOnChange={handleChange}
+        value={project.name ? project.name : ""} //se tiver categoria, pega o id, se nao, pega vazio
       />
       <Input
         type="number"
         text="Orçamento do projeto"
         name="budget"
         placeholder="Insira o orçamento total"
+        handleOnChange={handleChange}
+        value={project.budget ? project.budget : ""} //se tiver categoria, pega o id, se nao, pega vazio
       />
       <Select
         name="category_id"
         text="Selecione a categoria"
         options={categories}
+        handleOnChange={handleCategory}
+        value={project.category ? project.category.id : ""} //se tiver categoria, pega o id, se nao, pega vazio
       />
       <SubmitButton text={btnText} />
     </form>
